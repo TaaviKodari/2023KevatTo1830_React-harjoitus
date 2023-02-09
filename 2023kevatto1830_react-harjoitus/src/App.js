e66b4ba94da3e62bd21b743a5024acebae46bc0e
Your Name <you@example.com>
2023-02-09 19:31:06 +0200

    nappi aloitettu

---
+++ 2023kevatto1830_react-harjoitus/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Message} from "./pages/Message";
+import { Buttons} from "./pages/Buttons";
+
+function App() {
+  return (
+    <div >
+      <Message />
+      <Buttons/>
+    </div>
+  );
+}
+
+export default App;
